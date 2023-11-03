f7451c61017c73403cd58fa2ac193d4bae1ab009
Oleg Rybkin <olehrybkin@gmail.com>
2023-11-02 21:22:54 -0700

    add awr files

---
+++ stable_baselines3/awr/__init__.py
@@ -0,0 +1,4 @@
+from stable_baselines3.awr.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+from stable_baselines3.awr.awr import AWR
+
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "AWR"]
